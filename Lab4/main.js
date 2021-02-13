44fc4d47fe60a10a1b02e1cbe7074dafe8282814
Paulina <pauc@LAPTOP-C5TRASB4.localdomain>
2021-02-13 15:25:03 -0600

    Plantilla para el lab

---
+++ Lab4/main.js
@@ -0,0 +1,42 @@
+/**
+ * 1. 
+ * @input número pedido con un prompt
+ * @return Una tabla con los números del 1 al número dado con sus cuadrados y cubos. 
+ * Utiliza document.write para producir la salida
+ */
+
+
+/**
+ * 2. 
+ * @input Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria.
+ * @return  La página debe indicar si el resultado fue correcto o incorrecto, 
+ *          y el tiempo que tardó el usuario en escribir la respuesta.
+ */
+
+
+ /**
+ * 3. Contador
+ * @input Un arreglo de números
+ * @return La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo
+ */
+
+
+ /**
+ * 4. Promedios
+ * @input Un arreglo de arreglos de números.
+ * @return Un arreglo con los promedios de cada uno de los renglones de la matriz.
+ */
+
+
+ /**
+ * 5. Inverso
+ * @input Un número
+ * @return El número con sus dígitos en orden inverso.
+ */
+
+
+ /**
+ * 6. Crea una solución para un problema de tu elección
+ * @input 
+ * @return 
+ */
