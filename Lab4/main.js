ac772ebb60ca2a8154d0cd3a4e703a5feaa151e9
Paulina <pauc@LAPTOP-C5TRASB4.localdomain>
2021-02-15 01:43:57 -0600

    Funciones 4, 5 y 6

--- Lab4/main.js
+++ Lab4/main.js
@@ -3,6 +3,10 @@ function reload() {
     location.reload() 
 }
 
+function buttonRefresh(){
+    document.write("<div><button onclick='reload()'>Volver a la pagina de inicio</button></div>");
+}
+
 /**
  * 1. cuadrados y cubos
  * @input número pedido con un prompt
@@ -20,7 +24,7 @@ function cuadradosCubos() {
         document.write("</tr>");
     }
     document.write("</table>")
-    document.write("<div><button onclick='reload()'>Volver a la pagina de inicio</button></div>");
+    buttonRefresh();
 }
 
 
@@ -43,7 +47,7 @@ function suma() {
         document.write("<p>Respuesta incorrecta</p>")
         document.write("<p>Te tardaste "+(endTime-startTime)+" segundos</p>");
     }
-    document.write("<div><button onclick='reload()'>Volver a la pagina de inicio</button></div>");
+    buttonRefresh();
 }
 
 
@@ -70,26 +74,69 @@ function contador(arreglo) {
     document.write("<p><strong>Numeros negativos:</strong><span></span>",negativos,"</p>")
     document.write("<p><strong>Ceros:</strong><span></span>",ceros,"</p>")
     document.write("<p><strong>Numeros positivos:</strong><span></span>",positivos,"</p>")
-    document.write("<div><button onclick='reload()'>Volver a la pagina de inicio</button></div>");
+    buttonRefresh();
 }
 
 
- /**
+/**
+ * [ [3,8,1], [5,2,9], [4,0,2] ]
  * 4. Promedios
  * @input Un arreglo de arreglos de números.
  * @return Un arreglo con los promedios de cada uno de los renglones de la matriz.
  */
+function promedios(array) { 
+    let average = 0; 
+    document.write("<h1>Promedios</h1>","<p>[");
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            average += array[i][j];
+        }
+        if(i < array.length-1) document.write("&nbsp ",average/array.length,",");
+        else document.write("&nbsp ",average/array.length);
+        average = 0;
+    } 
+    document.write("]</p>");
+    buttonRefresh();
+}
 
 
- /**
+/**
  * 5. Inverso
  * @input Un número
  * @return El número con sus dígitos en orden inverso.
  */
-
-
+function inverso() { 
+    let numero = prompt("Ingrese un numero: ");
+    let respuesta = "";
+    for (let i = numero.length-1; i >= 0; i--) {
+        respuesta+=numero[i]; 
+    } 
+    document.write("<h1>Inverso</h1>");
+    document.write("<p>El inverso es: ",respuesta,"</p>");
+    buttonRefresh(); 
+} 
  /**
- * 6. Crea una solución para un problema de tu elección
+ * 6. Selection Sort
  * @input 
  * @return 
  */
+function  selectionSort(array) {
+    let pos = 0, aux;
+
+    for (let i = array.length-1; i > 0 ; i-- )  {
+        pos = 0 ;
+        for ( let j = 1 ; j <= i ; j++ ) {
+            if ( array[ j ] > array[pos] ) {
+                pos = j ;
+            }
+        }
+        if (pos != i) {
+            aux = array[pos];
+            array[pos] = array[i];
+            array[i] = aux;
+        }
+    }
+    document.write("<h1>Selection Sort</h1>");
+    document.write( "<p>El arreglo ordenado es: ",array,"</p>");
+    buttonRefresh()
+}
