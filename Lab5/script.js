db9afe1547500cd9565bd73ffe12bcac4c505163
Paulina <pauc@LAPTOP-C5TRASB4.localdomain>
2021-02-15 19:42:41 -0600

    Ejercicio 1

---
+++ Lab5/script.js
@@ -0,0 +1,13 @@
+const formulario = document.getElementById("formulario");
+formulario.onsubmit = function(){validacion()};
+
+function validacion() {
+    let pass1 = document.getElementById("password1").value;
+    let pass2 = document.getElementById("password2").value;
+    if (pass1 === pass2) {
+        alert("Contraseña validada")
+    }
+    else {
+        alert("No coinciden las contraseñas, inténtelo de nuevo")
+    }
+}
