3da0b2d5e823adb3f1cc5ae8fbc6f33a1dc853a4
Paulina <pauc@LAPTOP-C5TRASB4.localdomain>
2021-02-16 23:43:24 -0600

    Ultimos detalles

--- Lab5/5.1.js
+++ Lab5/5.1.js
@@ -4,10 +4,12 @@ formulario.onsubmit = function(){validacion()};
 function validacion() {
     let pass1 = document.getElementById("password1").value;
     let pass2 = document.getElementById("password2").value;
-    if (pass1 === pass2) {
-        alert("Contraseña validada")
+    if (pass1 === "") {
+        alert("Ingresa una contraseña");}
+    else if (pass1 === pass2) {
+        alert("Contraseña validada");
     }
     else {
-        alert("No coinciden las contraseñas, inténtelo de nuevo")
+        alert("No coinciden las contraseñas, inténtelo de nuevo");
     }
 }
