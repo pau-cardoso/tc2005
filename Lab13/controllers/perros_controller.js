99ebd148b2122460d7471cd6805316be5a6f683d
Pau <A01701490@itesm.mx>
2021-03-07 20:29:06 -0600

    cambios en la capa modelos y controlador

--- Lab13/controllers/perros_controller.js
+++ Lab13/controllers/perros_controller.js
@@ -1,16 +1,17 @@
-const perros = ['Boyo', 'Mina', 'Luke', 'Sam', 'Jade', 'Kira'];
-
+const Perro = require('../models/Perro');
 
 exports.getNuevoPerro = (request, response, next) => {
     response.render('nuevoPerro');
 };
 
 exports.postNuevoPerro = (request, response, next) => {
-    perros.push(request.body.nombre);
+    const nuevoPerro = new Perro(request.body.nombre);
+    nuevoPerro.save();
     response.redirect('/perros');
 }
 
 exports.get = (request, response, next) => {
+    const perros = Perro.fetchAll();
     response.render('perros', {
         perros: perros
     });
--- Lab13/models/Perro.js
+++ Lab13/models/Perro.js
@@ -1,3 +1,5 @@
+const perros = ['Boyo', 'Mina', 'Luke', 'Sam', 'Jade', 'Kira'];
+
 module.exports = class Perro {
 
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
@@ -7,12 +9,12 @@ module.exports = class Perro {
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        
+        perros.push(this.nombre);
     }
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
     static fetchAll() {
-        
+        return perros;
     }
 
 }
